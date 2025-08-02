af11c1f388625c5e0b27ba02440af79153564a68
alexreynlds <ajrey@live.co.uk>
2025-08-02 20:32:09 +0100

    + Implemented a basic login screen - lacks functionality for now

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,28 +1,45 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import Navbar from "@/components/navigation/Navbar";
+import { useState } from "react";
+
+
+export default function home() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("Username:", username);
+    console.log("Password:", password);
+  }
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <nav className="border-2 p-3 rounded-2xl w-[80%] flex justify-between">
-        <h1 className="flex items-center gap-4 text-3xl font-medium">
-          <a href="/" title="AGRIDS home page">
-            <img src="/img/agrids_logo_transparent_crop.png" alt="agrids logo" className="h-10 mt-[-5px]" />
-          </a>
-          AGRIDS MAP
-        </h1>
-        <ul className="flex gap-4 text-lg font-medium items-center">
-          <li className="bg-[#70B664] py-1 px-4 rounded-xl text-white hover:bg-[#5a9b52] transition-colors duration-300">
-            <a href="">HOME</a>
-          </li>
-          <li className="bg-[#70B664] py-1 px-4 rounded-xl text-white hover:bg-[#5a9b52] transition-colors duration-300">
-            <a href="">LOGIN</a>
-          </li>
-          <li className="bg-[#70B664] py-1 px-4 rounded-xl text-white hover:bg-[#5a9b52] transition-colors duration-300">
-            <a href="">REGISTER</a>
-          </li>
-        </ul>
-      </nav>
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 relative">
+      <div className="bg-[url(/img/field.jpg)] bg-cover blur-xs w-full h-full absolute" />
+      <main className="flex flex-col gap-[24px] row-start-2 items-center border-3 w-[25%] min-w-[350px] h-[100%] rounded-xl p-4 shadow-xl z-10 bg-white/70 backdrop-blur-md">
+        <img
+          src="/img/agrids_logo_transparent_crop.png"
+          alt="agrids logo"
+          className="h-10"
+        />
+        <h1 className="text-xl font-bold">LOGIN</h1>
+        <form className="flex flex-col  w-full h-full" onSubmit={handleLogin}>
+          <label className="text-sm text-gray-600">Username</label>
+          <input placeholder="Username" className="border-2 p-2 rounded-sm mb-2" onChange={(e) => setUsername(e.target.value)} value={username} />
+          <label className="text-sm text-gray-600">Password</label>
+          <input type="password" placeholder="Password" className="border-2 p-2 rounded-sm mb-2" onChange={(e) => setPassword(e.target.value)} value={password} />
+          <div className="flex items-center justify-between mt-2 mb-4">
+            <div className="flex items-center">
+              <input type="checkbox" id="remember" className="mr-2" />
+              <label htmlFor="remember" className="text-sm text-gray-600">Remember me</label>
+            </div>
+            <a href="#" className="text-sm text-blue-600 hover:underline">Forgot password?</a>
+          </div>
+          <button type="submit" className="mt-auto bg-[#70b664] py-1 px-4 rounded-sm text-white hover:bg-[#5a9b52] transition-colors duration-300 cursor-pointer">
+            Login
+          </button>
+        </form>
       </main>
 
     </div>
---
+++ frontend/components/navigation/Navbar.tsx
@@ -0,0 +1,31 @@
+// components/Navbar.jsx
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="border-2 p-3 rounded-2xl w-[80%] flex justify-between">
+      <h1 className="flex items-center gap-4 text-3xl font-medium">
+        <a href="/" title="agrids home page">
+          <img
+            src="/img/agrids_logo_transparent_crop.png"
+            alt="agrids logo"
+            className="h-10 mt-[-5px]"
+          />
+        </a>
+        agrids map
+      </h1>
+      <ul className="flex gap-4 text-lg font-medium items-center">
+        <li className="bg-[#70b664] py-1 px-4 rounded-xl text-white hover:bg-[#5a9b52] transition-colors duration-300">
+          <a href="">home</a>
+        </li>
+        <li className="bg-[#70b664] py-1 px-4 rounded-xl text-white hover:bg-[#5a9b52] transition-colors duration-300">
+          <a href="">login</a>
+        </li>
+        <li className="bg-[#70b664] py-1 px-4 rounded-xl text-white hover:bg-[#5a9b52] transition-colors duration-300">
+          <a href="">register</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
