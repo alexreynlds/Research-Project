6496e3afb77241646935c88a90f8e5003c3ba6a1
alexreynolds <ajrey@live.co.uk>
2025-08-14 17:47:23 +0100

    + more slight aesthetic changes

--- frontend/app/admin/page.jsx
+++ frontend/app/admin/page.jsx
@@ -4,19 +4,17 @@ import AdminButton from "@/components/admin/admin-button";
 import AdminUsersPage from "@/components/admin/pages/users";
 import useAuth from "@/components/auth/auth-context";
 import MainLayout from "@/components/layouts/main-layout";
-import { useRouter } from "next/navigation";
-import { useCallback, useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function Page() {
-  const router = useRouter();
-  const { user, signOut } = useAuth();
+  const { user } = useAuth();
   const [page, setPage] = useState(0);
 
   return (
     user && (
       <MainLayout pageTitle="Admin Dashboard">
-        <div className="border-2 w-full rounded-md p-3 flex flex-row items-center justify-between">
-          <ul className="flex gap-5">
+        <div className="border-2 w-full rounded-md p-3 flex flex-col gap-3 md:flex-row items-center justify-between">
+          <ul className="grid grid-cols-2 gap-2 sm:flex sm:flex-wrap sm:gap-3">
             <AdminButton page={0} currentPage={page} onClick={() => setPage(0)}>
               USERS
             </AdminButton>
@@ -31,7 +29,7 @@ export default function Page() {
             </AdminButton>
           </ul>
           <div>
-            <p className="font-bold">Welcome, {user.email}</p>
+            <p className="font-bold underline">Welcome, {user.email}</p>
           </div>
         </div>
         <div className="border-2 p-3 w-full h-full rounded-md">
